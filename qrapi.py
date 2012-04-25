abb1fdb5fafc0a881ff7e7fc3eb142cd1d74a11f
Matt Copperwaite <mattcopp@gmail.com>
2012-04-25 18:16:24 +0100

    Used more standard template rendering

--- qrapi.py
+++ qrapi.py
@@ -3,19 +3,22 @@ import os
 import jinja2
 from json import dumps as dict_to_json_string
 from urllib import quote_plus as urlencode, unquote as urldecode
-
+from google.appengine.ext.webapp import template
 from qrparse import GetQR
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class FrontPage(RequestHandler):
+    def render(self, template_file, template_values=None):
+        path = os.path.join(os.path.dirname(__file__), 'templates', template_file)
+        self.response.out.write(template.render(path, template_values))
+
     def get(self):
         if self.request.get('value'):
             self.redirect(uri_for('qrimage', value=urlencode(self.request.get('value'))))
         else:
-            template = jinja_environment.get_template('frontpage.html')
-            self.response.out.write(template.render())
+            self.render('frontpage.html')
 
 class QRImage(RequestHandler):
     def get(self, value):
