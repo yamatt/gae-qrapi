8ee317baea856d316b4101b27cc365e701c20ef9
Matt Copperwaite <mattcopp@gmail.com>
2012-04-28 18:22:08 +0100

    Fixed some bugs in the back end

--- qrapi.py
+++ qrapi.py
@@ -7,31 +7,42 @@ from google.appengine.ext.webapp import template
 from qrparse import GetQR
 
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")))
+
+def jsonify(response, out_dict):
+    out_json = dict_to_json_string(out_dict)
+    response.headers['Content-Type'] = "application/json"
+    response.out.write(out_json)
+
+def render(response, template_file, template_values=None):
+    template = jinja_environment.get_template(template_file)
+    response.out.write(template.render(template_values))
 
 class FrontPage(RequestHandler):
-    def render(self, template_file, template_values=None):
-        path = os.path.join(os.path.dirname(__file__), 'templates', template_file)
-        self.response.out.write(template.render(path, template_values))
-
     def get(self):
-        if self.request.get('value'):
+        if self.request.get('value') and len(self.request.get('value')) >= 6:
             self.redirect(uri_for('qrimage', value=urlencode(self.request.get('value'))))
         else:
-            self.render('frontpage.html')
+            render(self.response, 'frontpage.html', {'value': self.request.get('value')})
 
 class QRImage(RequestHandler):
     def get(self, value):
         value = urldecode(value)
         qr = GetQR(value)
-        png = qr.get_image()
-#        self.response.headers['Content-Type'] = "plain/text"
-        self.response.headers['Content-Type'] = "image/png"
-        self.response.out.write(png)
+        errors = qr.has_errors()
+        if errors:
+            jsonify(self.response, {"success": False, "message": errors})
+        else:
+            png = qr.get_image()
+            if self.request.get('info'):
+                jsonify(self.response, {"success": True, "message": "Image generated. Saved to database."})
+            else:
+                self.response.headers['Content-Type'] = "image/png"
+                self.response.out.write(png)
 
 app = WSGIApplication([
         Route('/', handler=FrontPage, name='home'),
-        Route('/<value>', handler=QRImage, name='qrimage')
+        Route('/<value:.*>', handler=QRImage, name='qrimage')
     ],
     debug=True
 )
--- qrparse.py
+++ qrparse.py
@@ -1,16 +1,29 @@
 from pyqrcode import MakeQR
 from StringIO import StringIO
 from png import Writer as PNGWriter
+from hashlib import sha512
+from datetime import datetime
+import logging
 
 from google.appengine.api.images import Image
 from google.appengine.ext import db
 
+class QRCode(db.Model):
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_used = db.DateTimeProperty(auto_now=True)
+    image = db.BlobProperty()
+    version = db.IntegerProperty(default=1)
+
 class GetQR(object):
-    def __init__(self, value, type=None):
+    def __init__(self, value):
         self.value = value
-        self.type = type
+        self.hash_key = sha512(value).hexdigest()
         
-    def get_image(self, size=300):
+    def has_errors(self):
+        if len(self.value) < 6:
+            return "Value specified is too short. It must be 6 characters or longer."
+        
+    def make_image(self, size=300):
     	qr = MakeQR(self.value)
     	nodes = map(lambda row: map(lambda data: False if data else True, row), qr.modules)
     	rows = len(nodes)
@@ -20,7 +33,42 @@ class GetQR(object):
     	w = PNGWriter(rows, columns, greyscale=True, bitdepth=1)
     	w.write(buff, nodes)
     	qr_image = buff.getvalue()
-    	image = Image(image_data=qr_image)
+    	return qr_image
+        
+    def store_new_image(self, image_obj):
+        qr_store = QRCode(key_name=self.hash_key, image=image_obj)
+        qr_store.put()
+        
+    def access_image(self):
+        """
+        Get the image, update the last access time and return image
+        """
+        k = db.Key.from_path("QRCode", self.hash_key)
+        qr_store = db.get(k)
+        qr_image = qr_store.image
+        qr_store.put()
+        return qr_image
+        
+    def has_image(self):
+        """
+        returns true if entry exists for qr
+        """
+        k = db.Key.from_path("QRCode", self.hash_key)
+        return (db.get(k))
+        
+    def scale_image(self, image_obj, size=300):
+    	image = Image(image_data=image_obj)
     	image.resize(height=size)
     	image.execute_transforms()
-    	return image._image_data
+        return image._image_data
+        
+    def get_image(self):
+        if self.has_image():
+            logging.info("Has image for: %s" % self.value)
+            qr_image = self.access_image()
+        else:
+            logging.info("Generating image: %s" % self.value)
+            qr_image = self.make_image()
+            self.store_new_image(qr_image)
+        qr_image = self.scale_image(qr_image)
+        return qr_image
