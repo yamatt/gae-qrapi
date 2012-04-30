8f35e1c76e6513b48ea3948ad0992b65d61272d7
Matt Copperwaite <mattcopp@gmail.com>
2012-04-30 19:38:07 +0100

    Added bug fix for url in value

--- qrparse.py
+++ qrparse.py
@@ -8,6 +8,21 @@ import logging
 from google.appengine.api.images import Image
 from google.appengine.ext import db
 
+#~ This file is part of QR API.
+#~ 
+#~ QR API is free software: you can redistribute it and/or modify
+#~ it under the terms of the GNU General Public License as published by
+#~ the Free Software Foundation, either version 3 of the License, or
+#~ (at your option) any later version.
+#~ 
+#~ QR API is distributed in the hope that it will be useful,
+#~ but WITHOUT ANY WARRANTY; without even the implied warranty of
+#~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#~ GNU General Public License for more details.
+#~ 
+#~ You should have received a copy of the GNU General Public License
+#~ along with QR API.  If not, see <http://www.gnu.org/licenses/>.
+
 class QRCode(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
     last_used = db.DateTimeProperty(auto_now=True)
