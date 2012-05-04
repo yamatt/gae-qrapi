d15e37c52739d0ecf142cdbadb0e9196985d7e5a
Matt Copperwaite <mattcopp@gmail.com>
2012-05-04 22:18:05 +0100

    Added more error messages

--- qrapi.py
+++ qrapi.py
@@ -20,7 +20,7 @@ import jinja2
 from json import dumps as dict_to_json_string
 from urllib import quote_plus as urlencode, unquote as urldecode
 from google.appengine.ext.webapp import template
-from qrmodels import QRKey, QRStore
+from qrmodels import QRKey, QRStore, QRValueError
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")))
@@ -45,13 +45,20 @@ class QRImage(RequestHandler):
     def get(self, value):
         value = urldecode(value)
         qr_key = QRKey.from_value(value)
-        qr_image = QRStore.get(qr_key)
-        if not qr_image:
-            qr_image = QRStore(value)
-        
-        qr_image.put()  # saves the image if new, but otherwise just updates the access time
-        self.response.headers['Content-Type'] = "image/png"
-        self.response.out.write(qr_image.get_image())
+        try:
+            qr_image = QRStore.get(qr_key)
+            if not qr_image:
+                qr_image = QRStore(value)
+            
+            qr_image.put()  # saves the image if new, but otherwise just updates the access time
+            if self.request.get('info'):
+                jsonify(self.response, {"success": True, "message": "QR code now stored in database."})
+            else:
+                self.response.headers['Content-Type'] = "image/png"
+                self.response.out.write(qr_image.get_image())
+                
+        except QRValueError:
+            jsonify(self.response, {"success": False, "message": "Value too short."})
         
 
 app = WSGIApplication([
